edc6aaf23ad14d7c37063c35927feb6f52de63e0
myUsername <myemail@gmail.com>
2023-09-19 06:57:46 -0700

    un

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "mainy.h"
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point, generates random passwords for 101-crackme
+ * Return: 0 on success
+ */
+int main(void)
+{
+	/* ... your logic for generating passwords ... */
+
+	return (0);
+}
+
+/**
+ * function_name - Short description of the function
+ * @parameter: Description of parameter
+ * Description: Longer description of the function, if necessary
+ * Return: What does the function return? (if anything)
+ */
---
+++ 0x05-pointers_arrays_strings/mainy.h
@@ -0,0 +1,9 @@
+#ifndef MAINY_H
+#define MAINY_H
+
+#include <stdio.h>
+
+/* Function prototypes here */
+void your_function_name_here(void);
+
+#endif /* MAINY_H */
