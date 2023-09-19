4878bb393dd93733e5bcb2552bb78e6623f2f843
myUsername <myemail@gmail.com>
2023-09-19 14:54:24 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all single digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
