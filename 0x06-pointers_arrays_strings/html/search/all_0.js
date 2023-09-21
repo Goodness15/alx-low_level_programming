565ededf62d3b903363df080f0d0e7b757567bca
myUsername <myemail@gmail.com>
2023-09-20 19:01:17 -0700

    welcome

--- 0x06-pointers_arrays_strings/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['0_2dstrcat_2ec_0',['0-strcat.c',['../0-strcat_8c.html',1,'']]]
-];
--- 0x06-pointers_arrays_strings/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_5fstrcat_3',['_strcat',['../0-strcat_8c.html#a201f80b4a1a47920454f9decc935cf35',1,'0-strcat.c']]]
-];
