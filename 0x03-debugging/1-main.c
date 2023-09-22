77280cd89f12a3cb481f81d938181930e7c7662e
myUsername <myemail@gmail.com>
2023-09-22 07:01:23 -0700

    Happy day

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Description: This function was originally designed to cause
+* an infinite loop but has been commented out to prevent it.
+* Return: 0 if successful, non-zero otherwise.
+*/
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
