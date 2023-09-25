a4b3d7d10d7d3cb78fef670cfdb0ecc941695224
myUsername <myemail@gmail.com>
2023-09-25 16:06:49 -0700

    MY JESUS

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix
- * @a: the start of the matrix
- * @size: the size of one side of the matrix
- * 
- * Return: void
- */
+* print_diagsums - prints the sum of the two diagonals of a square matrix
+* @a: the start of the matrix
+* @size: the size of one side of the matrix
+*
+* Return: void
+*/
 void print_diagsums(int *a, int size)
 {
-    int i;
-    int sum1 = 0, sum2 = 0;
+int i;
+int sum1 = 0, sum2 = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        sum1 += a[(size + 1) * i];      /* Incrementing by size+1 goes down the main diagonal */
-        sum2 += a[(size - 1) * (i + 1)];  /* Incrementing by size-1 goes down the secondary diagonal */
-    }
-    printf("%d, %d\n", sum1, sum2);
+for (i = 0; i < size; i++)
+{
+sum1 += a[(size + 1) * i];	/* Incrementing by size+1 goes down agonal */
+sum2 += a[(size - 1) * (i + 1)]; /* Incrementing by size-1 dary gonal */
+}
+printf("%d, %d\n", sum1, sum2);
 }
