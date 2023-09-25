cfad405e50731a3808feee47dd7aec3f81fcc8e2
myUsername <myemail@gmail.com>
2023-09-25 14:58:42 -0700

    ONLY GOD CAN DO THIS

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Function prototypes */
+int _putchar(char c);
+void print_number(int n);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif /* MAIN_H */
+
